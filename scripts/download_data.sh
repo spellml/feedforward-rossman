27551104b74eed00294d2b75691b06fdaf89b6b7
Aleksey Bilogur <aleksey@residentmar.io>
2020-05-26 21:52:06 +0000

    Bugfix: download the right dataset.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,4 +1,4 @@
 export KAGGLE_USERNAME=residentmario KAGGLE_KEY=ea4163444bc3b21c1b810b218c385038
-kaggle competitions download rossmann-store-sales
-unzip rossmann-store-sales.zip -d /mnt/rossmann-store-sales/
-rm rossmann-store-sales.zip
+kaggle datasets download residentmario/rossman-fastai-sample
+unzip rossman-fastai-sample.zip -d /mnt/rossman-fastai-sample/
+rm rossman-fastai-sample.zip
